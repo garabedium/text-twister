82b08efea5fb6697dfb719a46918b329bd8af39c
Greg G <garabedium@users.noreply.github.com>
2023-01-20 13:22:28 -0500

    add Timer reset test

--- src/js/components/Timer/Timer.jsx
+++ src/js/components/Timer/Timer.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { TimerDev, TimerProd, IsDevEnv } from '../../utils/constants';
+import {
+  TimeDev, TimeProd, IsDevEnv, Icons,
+} from '../../utils/constants';
 import Button from '../Button/Button';
 import './Timer.scss';
 
 function Timer(props) {
   const { updateGameState, restartGame, game } = props;
-  const startTime = IsDevEnv ? TimerDev : TimerProd;
+  const startTime = IsDevEnv ? TimeDev : TimeProd;
   const [seconds, setSeconds] = useState(startTime);
 
-  // cases & props:
-  // if player.solvedAll is true, terminate the timer, set seconds to 0
+  // TODO: player.solvedAll, reset timer.
 
   useEffect(() => {
     let interval = null;
@@ -36,7 +37,7 @@ function Timer(props) {
     <div className="game-timer">
       {game.reset ? (
         <Button
-          icon="ri-play-fill ri-2x"
+          icon={`${Icons.play_fill} ri-2x`}
           handleClick={() => restartGame()}
         />
       ) : <span>{seconds}</span>}
--- src/js/components/Timer/Timer.test.jsx
+++ src/js/components/Timer/Timer.test.jsx
@@ -1,8 +1,15 @@
-// import React from 'react';
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { ResetGame } from '../../utils/test-utils';
+import { Icons } from '../../utils/constants';
+import Timer from './Timer';
 
-// describe('Timer component', () => {
-//   it('counts down to 0', () => {
-//   });
-//   it('resets after ...', () => {
-//   })
-// });
+describe('Timer component', () => {
+  it('should display a reset button when the game is reset', () => {
+    const { container } = render(
+      <Timer game={ResetGame} updateGameState={jest.fn()} />,
+    );
+    expect(container.getElementsByClassName(Icons.play_fill).length).toBe(1);
+  });
+});
