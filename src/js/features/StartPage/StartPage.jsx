39e1368bcee5b07da123c2333845db49d4b16f61
Greg G <garabedium@users.noreply.github.com>
2023-02-27 18:12:28 -0500

    fix start page styles

--- src/js/features/StartPage/StartPage.jsx
+++ src/js/features/StartPage/StartPage.jsx
@@ -1,57 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import './StartPage.scss';
-// import { shuffleLetters } from '../../utils/utils';
 import { GameStates, Icons } from '../../utils/constants';
 
 import Button from '../../components/Button/Button';
 import GameWord from '../../components/GameWord/GameWord';
 
 function StartPage({ updateGameStatus, hasLevelWord }) {
-  // const solvedWord = 'play';
-  // const shuffleLimit = 5;
-  // const shuffleDelay = 1000;
-  const displayWord = 'play';
-
-  // const [shuffleWord, setShuffleWord] = useState('');
-  // const [shuffleCount, setShuffleCount] = useState(0);
-
-  // Shuffle "play" letters a few times:
-  // function shuffleStartPageLetters() {
-  //   const currentShuffle = shuffleLetters(solvedWord, '');
-
-  //   if (currentShuffle === solvedWord || currentShuffle === shuffleWord) {
-  //     return shuffleStartPageLetters();
-  //   }
-
-  //   if (shuffleCount < shuffleLimit) {
-  //     setShuffleWord(currentShuffle);
-  //     setShuffleCount((shuffleCount) => shuffleCount + 1);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   shuffleStartPageLetters();
-  // }, []);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     shuffleStartPageLetters();
-  //   }, shuffleDelay);
-  // }, [shuffleCount, shuffleWord]);
-
-  // const displayWord = shuffleCount === shuffleLimit ? solvedWord : shuffleWord;
-  // const letters = displayWord.split('').map((letter) => (
-  //   <span className="letter text-uppercase" key={letter}>{letter}</span>
-  // ));
-
-  // <Button
-  //   class="letter text-uppercase"
-  //   text={letter}
-  //   key={letter}
-  // />
   return (
-    <div className="word-row start-page">
+    <div className="start-page">
       <div className="game-timer">
         {hasLevelWord
           && (
@@ -62,7 +19,7 @@ function StartPage({ updateGameStatus, hasLevelWord }) {
           )}
       </div>
       <GameWord
-        word={displayWord}
+        word="play"
       />
       <ul className="instructions-list">
         <li>
