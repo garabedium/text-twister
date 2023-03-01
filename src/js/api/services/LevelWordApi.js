32adf464b3c7c24fc41f7ca5904feb7721b2d77d
Greg G <garabedium@users.noreply.github.com>
2023-03-01 17:14:09 -0500

    fix App tests

--- src/js/api/services/LevelWordApi.js
+++ src/js/api/services/LevelWordApi.js
@@ -4,8 +4,6 @@ import { ApiRoutes } from '../../utils/constants';
 const LevelWordApi = {
   getByRange: (min, max, usedWords) => client.get(`${ApiRoutes.levelWordRange}/${min}&${max}?${usedWords}`),
   getAnagrams: (word) => client.get(`${ApiRoutes.anagrams}/${word}`),
-  // validate: (word) => client.get(`word/validate/${word}`),
-  // validate: async (word) => client.get(`word/validate/${word}`),
 };
 
 export default LevelWordApi;
--- src/js/features/App/App.test.jsx
+++ src/js/features/App/App.test.jsx
@@ -1,35 +1,33 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import nock from 'nock';
-import { render, waitFor } from '@testing-library/react';
-import { Icons, ApiRoutes } from '../../utils/constants';
+import { render, waitFor, screen } from '@testing-library/react';
+import {
+  ApiRoutes, ZipfDefaultMin, ZipfDefaultMax, PlayButtonText,
+} from '../../utils/constants';
+import { nockGetRequest } from '../../utils/test-utils';
 import App from './App';
 
 describe('App component', () => {
-  it('should render the game instructions', async () => {
-    const { container } = render(<App />);
-    const icons = [
-      Icons.timer_flash, Icons.funds, Icons.warning, Icons.spacebar, Icons.arrow_right,
-    ];
+  const playButton = () => screen.getByLabelText(PlayButtonText);
+  const levelWordRequest = `${ApiRoutes.levelWordRange}/${ZipfDefaultMin}&${ZipfDefaultMax}`;
 
-    icons.forEach((icon) => {
-      expect(container.getElementsByClassName(icon).length).toBe(1);
+  it('should load the play button', async () => {
+    nockGetRequest(levelWordRequest, [{ word: 'texter' }]);
+    render(<App />);
+
+    expect(playButton()).toBeDisabled();
+
+    await waitFor(() => {
+      expect(playButton()).toBeEnabled();
     });
   });
-  // it('should load the play button', async () => {
-  //   nock(ApiRoutes.baseUrl)
-  //     .defaultReplyHeaders({
-  //       'access-control-allow-origin': '*',
-  //       'access-control-allow-credentials': 'true',
-  //     })
-  //     .get(ApiRoutes.levelWordRange)
-  //     .reply(200, [{ word: 'texter' }]);
 
-  //   render(<App />);
+  it('should not load the play button', async () => {
+    nockGetRequest(levelWordRequest, [{ word: '' }]);
+    render(<App />);
 
-  //   await waitFor(() => {
-  //     // TODO: add aria-label to button, check for button label:
-  //     expect(document.getElementsByClassName(Icons.play_fill).length).toBe(1);
-  //   });
-  // });
+    await waitFor(() => {
+      expect(playButton()).toBeDisabled();
+    });
+  });
 });
