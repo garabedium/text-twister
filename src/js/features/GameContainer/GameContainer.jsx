fc7cd06c1d0585024d0e0bf06b30aaab5c1a2341
Greg G <garabedium@users.noreply.github.com>
2023-03-06 15:54:48 -0500

    ignore coverage folder

--- src/js/features/GameContainer/GameContainer.jsx
+++ src/js/features/GameContainer/GameContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import isTouchDevice from 'is-touch-device';
 
 import './GameContainer.scss';
