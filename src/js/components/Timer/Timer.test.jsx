2637feeef083bcf97482f8e58eddedc7e4086ddc
Greg G <garabedium@users.noreply.github.com>
2023-03-09 11:06:56 -0500

    test timer countdown

--- src/js/components/Timer/Timer.test.jsx
+++ src/js/components/Timer/Timer.test.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { screen, render } from '@testing-library/react';
+import {
+  screen, render, act,
+} from '@testing-library/react';
 import { GameStates, PlayButtonText, TimeDev } from '../../utils/constants';
 import Timer from './Timer';
 
@@ -27,5 +29,18 @@ describe('Timer component', () => {
     );
     const playButton = screen.getByLabelText(PlayButtonText);
     expect(playButton).toBeInTheDocument();
+  });
+
+  it('should count down from a specific number to zero', async () => {
+    jest.useFakeTimers();
+    render(<Timer
+      gameStatus={GameStates.active}
+      updateGameStatus={() => null}
+    />);
+    expect(screen.getByText(TimeDev)).toBeInTheDocument();
+
+    act(() => jest.advanceTimersByTime(TimeDev * 1000));
+
+    expect(await screen.findByText(TimeDev - TimeDev)).toBeInTheDocument();
   });
 });
